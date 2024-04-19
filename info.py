85b87c71cfd090b5ec1bbd670f395954fcca4016
Pavan6264a <127077968+Pavan6264a@users.noreply.github.com>
2024-04-19 15:48:36 +0530

    Update info.py

--- info.py
+++ info.py
@@ -53,7 +53,7 @@ DATABASE_NAME = environ.get('DATABASE_NAME', "vkbotsthalpathy")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # Others
-VERIFY = bool(environ.get('VERIFY', True))
+VERIFY = bool(environ.get('VERIFY', False))
 SHORTLINK_URL = environ.get('SHORTLINK_URL', 'youtube.com')
 SHORTLINK_API = environ.get('SHORTLINK_API', 'AIzaSyC1c65REhCgDxV59w5TJujCptV-knsiHHU')
 IS_SHORTLINK = bool(environ.get('IS_SHORTLINK', False))
