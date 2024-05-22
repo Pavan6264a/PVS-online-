6dc471a2f8810f8d6d2b2adb59bcb7581068d4ad
Pavan6264a <127077968+Pavan6264a@users.noreply.github.com>
2024-05-22 19:27:04 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,6 @@
 # Don't Remove Credit @VJ_Botz
 # Subscribe YouTube Channel For Amazing Bot @Tech_VJ
 # Ask Doubt on telegram @KingVJ01
-
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
@@ -11,6 +10,5 @@ else
   git clone $UPSTREAM_REPO /VJ-FILTER-BOT 
 fi
 cd /VJ-FILTER-BOT 
-pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
